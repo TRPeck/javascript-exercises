1b846feb0f26ee8da6673098915a4b82107674dd
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-05 15:04:19 -0700

    Do exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let repeatedStr = "";
+    if(n < 0) {
+        repeatedStr = "ERROR";
+    }
+    else {
+        for(let i = 0; i < n; i++) {
+            repeatedStr += str;
+        }
+    }
 
+    return repeatedStr;
 };
 
 // Do not edit below this line
