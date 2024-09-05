96456fb7701a42caa70c14c7f2849a1ac073657c
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-05 16:53:57 -0700

    Do exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    for(let j = 1; j < args.length; j++) {
+        for(let i = 0; i < args[0].length; i++) {
+            if(args[0][i] === args[j]) {
+                args[0].splice(i, 1);
+                i -= 1;
+            }
+        }
+    }
+
+    return args[0];
 };
 
 // Do not edit below this line
