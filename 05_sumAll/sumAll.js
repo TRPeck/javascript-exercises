71ff0fc32df8f3393891351e1ee4c0fb4557e8e0
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-06 11:28:02 -0700

    Do exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(x < 0 || y < 0 || x % 1 != 0 || y % 1 != 0 || typeof(x) != "number" || typeof(y) != "number") {
+        return "ERROR";
+    }
+    else {
+        let sum = 0;
+        if(x == y) {
+            sum = x + y;
+        }
+        else if (x < y) {
+            for(let i = x; i <= y; i++) {
+                sum += i;
+            }
+        }
+        else if (y < x) {
+            for(let i = y; i <= x; i++) {
+                sum += i;
+            }
+        }
+    
+        return sum;
+    }
 };
 
 // Do not edit below this line
