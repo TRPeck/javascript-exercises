347b557ed88d7281c2d6ceb3899d49ea608e3e0c
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-06 11:58:43 -0700

    Do exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  if(celsius % 1 != 0) { celsius = Number.parseFloat(celsius.toFixed(1)); }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  if(fahrenheit % 1 != 0) { fahrenheit = Number.parseFloat(fahrenheit.toFixed(1)); }
+  return fahrenheit;
 };
 
 // Do not edit below this line
