8ca1128cc879ca0de417eb39cbad31bb2e9a7276
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-09 14:50:26 -0700

    Do exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
 const palindromes = function () {
-
+    const string = Array.from(arguments[0]);
+    const validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const noPuncArray = string.filter((ch) => validChars.includes(ch));
+    const lowerCaseArray = noPuncArray.map((ch) => ch.toLowerCase());
+    const original = [...lowerCaseArray];
+    const reversed = lowerCaseArray.reverse();
+    return original.every((value, index) => value === reversed[index]);
 };
 
 // Do not edit below this line
