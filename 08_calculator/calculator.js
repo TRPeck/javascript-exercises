a72a47a9d25c9282a5e566f5a4c12b20d5afef2a
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-09 13:46:12 -0700

    Do exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	const arr = Array.from(arguments[0]);
+  return arr.reduce((total, currentItem) => total + currentItem, 0);
 };
 
 const multiply = function() {
-
+  const arr = Array.from(arguments[0]);
+  return arr.reduce((total, currentItem) => total * currentItem);
 };
 
 const power = function() {
-	
+  return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	if(arguments[0] == 0) {
+    return 1;
+  }
+  else {
+    return arguments[0] * factorial(arguments[0] - 1);
+  }
 };
 
 // Do not edit below this line
