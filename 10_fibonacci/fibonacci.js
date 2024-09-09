b38339ef44aff62d2a3af83a7babda5ae90432de
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-09 15:35:43 -0700

    Do exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) { return "OOPS"; }
+    else {
+        const arr = [0, 1];
+        for(let i = 2; i <= num; i++) {
+            arr.push(arr[i-1] + arr[i-2]);
+        }
+        return arr[num];
+    }
 };
 
 // Do not edit below this line
